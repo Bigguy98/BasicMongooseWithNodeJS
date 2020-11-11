703ef2cd5f47995e8857600a633b22a70a52017f
vuthanhtung <vuthanhtunghayquen98@gmail.com>
2020-11-11 16:18:06 +0700

    add some comment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 4000;
 const router = require("./router/main")
 
 
-
+// connect to mongoDb
 const uri = "mongodb+srv://123456aA:123456aA@cluster0.wniry.mongodb.net/example?retryWrites=true&w=majority";
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 const connection = mongoose.connection;
@@ -14,7 +14,9 @@ connection.once("open", function() {
   console.log("MongoDB database connection established successfully");
 });
 
+// to parse request body
 app.use(bodyParser.json());
+// set router for app
 app.use("/api/employee", router);
 
 app.listen(port, function() {
